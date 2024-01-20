a1605887014e94c21a1afc647614ffd2095d6826
AustinYarborough <141944747+AustinYarborough@users.noreply.github.com>
2024-01-20 02:35:38 -0600

    Update movies.router.js

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { get } = require("request");
+
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
